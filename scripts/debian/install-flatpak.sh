1d8319a2af884a3cddb11380174755b7b27f36ff
Yağızhan <yagizhan49@protonmail.com>
2022-09-02 00:51:23 +0300

    Create install-flatpak.sh

---
+++ scripts/debian/install-flatpak.sh
@@ -0,0 +1,11 @@
+# https://github.com/evrifaessa/dotfiles
+# Install Flatpak and set up the Flathub repository
+# Should work on Ubuntu 18.10 (Cosmic Cuttlefish) or later
+
+# Install Flatpak
+sudo apt install flatpak
+
+# Set up the Flathub repository
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Now go ahead and restart your PC. I won't do that automatically.
